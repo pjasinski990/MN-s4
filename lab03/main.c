2ac52aeffbe21c9d99286ec22021c82457cdca32
Piotr Jasinski <piotr.jasinski990@gmail.com>
2020-03-24 20:01:27 +0100

    timers

--- lab03/main.c
+++ lab03/main.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include "nrutil.h"
 #include "nrutil.c"
+#include "gaussj.c"
 
 #define N 1000
 #define argM 5
 
-void conjgrad(double** A, double* x, double* b, int size);
-double scalar_prod(double* v, double* w, int n);
-double* vector_sum_scaled(double* v, double* w, double scale, int n);
-double* matrix_vector_prod(double** m, double* v, int n);
+void conjgrad(float** A, float* x, float* b, int size);
+float scalar_prod(float* v, float* w, int n);
+float* vector_sum_scaled(float* v, float* w, float scale, int n);
+float* matrix_vector_prod(float** m, float* v, int n);
 
 int main(int argc, const char *argv[]) {
-	double** m = dmatrix(0, N-1, 0, N-1);
-	double* b = dvector(0, N-1);
-	double* x = dvector(0, N-1);
+	float** m = matrix(1, N, 1, N);
+	float* b = vector(1, N);
+	float* x = vector(1, N);
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		x[i] = 0.0;
-		b[i] = i+1.0;
-		for (int j = 0; j < N; j++) {
+		b[i] = i;
+		for (int j = 1; j <= N; j++) {
 			if (abs(i-j) <= argM) {
 				m[i][j] = 1.0 / (1+abs(i-j));
 			} else {
@@ -28,23 +30,41 @@ int main(int argc, const char *argv[]) {
 		}
 	}
 
-	conjgrad(m, x, b, N);
+	// Calculating times
+	clock_t t1g, t2g, t1cg, t2cg;
+	float tg, tcg;
 
-	free_dmatrix(m, 0, N-1, 0, N-1);
-	free_dvector(x, 0, N-1);
-	free_dvector(b, 0, N-1);
+	t1cg = clock();
+	conjgrad(m, x, b, N);
+	t2cg = clock();
+	tcg = ((float)(t2cg-t1cg))/CLOCKS_PER_SEC;
+	// Comment since there to have proper output for graphing
+	printf("tcg is: %lf\n", tcg);
+
+	float** tempM = matrix(1, N, 1, N);
+	t1g = clock();
+	gaussj(m, N, tempM, 1);
+	t2cg = clock();
+	tcg = ((float)(t2cg-t1cg))/CLOCKS_PER_SEC;
+	printf("tcg is: %lf\n", tcg);
+	free_matrix(tempM, 1, N, 1, N);
+	// Comment till there to have proper output for graphing
+
+	free_matrix(m, 1, N, 1, N);
+	free_vector(x, 1, N);
+	free_vector(b, 1, N);
 	return 0;
 }
 
-void conjgrad(double** A, double* x, double* b, int size) {
-	double* v = matrix_vector_prod(A, x, size);
-	double* r = dvector(0, size-1);
-	for (int i = 0; i < size; i++) {
+void conjgrad(float** A, float* x, float* b, int size) {
+	float* v = matrix_vector_prod(A, x, size);
+	float* r = vector(1, size);
+	for (int i = 1; i <= size; i++) {
 		r[i] = v[i] = b[i] - v[i];
 	}
 
 	int k = 0;
-	double alpha, beta, old_scalar_rr;
+	float alpha, beta, old_scalar_rr;
 	alpha = beta = old_scalar_rr = 0.0;
 
 	printf("%d %f %f %f %f\n", k, sqrt(scalar_prod(r, r, size)), alpha, beta, sqrt(scalar_prod(x, x, size)));
@@ -52,56 +72,56 @@ void conjgrad(double** A, double* x, double* b, int size) {
 	while (sqrt(scalar_prod(r, r, size)) > 1e-6) {
 		old_scalar_rr = scalar_prod(r, r, size);
 
-		double* avprod = matrix_vector_prod(A, v, size);
+		float* avprod = matrix_vector_prod(A, v, size);
 		alpha = old_scalar_rr / scalar_prod(v, avprod, size);
 		
-		double* temp = vector_sum_scaled(x, v, alpha, size);
-		for (int i = 0; i < size; i++) {
+		float* temp = vector_sum_scaled(x, v, alpha, size);
+		for (int i = 1; i <= size; i++) {
 			x[i] = temp[i];		
 		}
-		free_dvector(temp, 0, size-1);
+		free_vector(temp, 1, size);
 
 		temp = vector_sum_scaled(r, avprod, -alpha, size);
-		free_dvector(r, 0, size-1);
-		free_dvector(avprod, 0, size-1);
+		free_vector(r, 1, size);
+		free_vector(avprod, 1, size);
 		r = temp;
 
 		beta = scalar_prod(r, r, size)/old_scalar_rr;
 
 		temp = vector_sum_scaled(r, v, beta, size);
-		free_dvector(v, 0, size-1);
+		free_vector(v, 1, size);
 		v = temp;
 
 		++k;
 		printf("%d %f %f %f %f\n", k, sqrt(scalar_prod(r, r, size)), alpha, beta, sqrt(scalar_prod(x, x, size)));
 	}
-	free_dvector(v, 0, size-1);
-	free_dvector(r, 0, size-1);
+	free_vector(v, 1, size);
+	free_vector(r, 1, size);
 }
 
-double scalar_prod(double* v, double* w, int n) {
-	double temp = 0;
-	for (int i = 0; i < n; i++) {
+float scalar_prod(float* v, float* w, int n) {
+	float temp = 0;
+	for (int i = 1; i <= n; i++) {
 		temp += v[i]*w[i];
 	}
 	return temp;
 }
 
-double* matrix_vector_prod(double** m, double* v, int n) {
-	double* result = dvector(0, n-1);
+float* matrix_vector_prod(float** m, float* v, int n) {
+	float* result = vector(1, n);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		result[i] = 0;
-		for (int j = 0; j < n; j++) {
+		for (int j = 1; j <= n; j++) {
 			result[i] += m[i][j]*v[j];
 		}
 	}
 	return result;
 }
 
-double* vector_sum_scaled(double* v, double* w, double scale, int n) {
-	double* result = dvector(0, n-1);
-	for (int i = 0; i < n; i++) {
+float* vector_sum_scaled(float* v, float* w, float scale, int n) {
+	float* result = vector(1, n);
+	for (int i = 1; i <= n; i++) {
 		result[i] = v[i] + w[i]*scale;
 	}
 	return result;
